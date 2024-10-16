ba20d492a1bafa289e784a2f391d702ffdd7aac4
euno4h <163136318+euno4h@users.noreply.github.com>
2024-10-16 09:19:39 -0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+let valorContador= 0;
+
+function incrementar(){
+    valorContador++;
+    document.getElementById('contador').innerText= valorContador;
+}
+function diminuir(){
+    valorContador--;
+    document.getElementById('contador').innerText= valorContador;
+}
